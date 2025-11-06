2b7cbc1398117caeac93b906a5ff334aad0d7e98
md-alamin2 <mdalamin22671@gmail.com>
2025-11-06 23:41:27 +0600

    learn aobut class

--- class.js
+++ class.js
@@ -13,3 +13,26 @@
 
 // console.log(myCounter(2));
 // console.log(myCounter(7))
+
+// class
+
+class Counter{
+    constructor(count){
+        this.count = count;
+    }
+
+    add(amount){
+        this.count = this.count + amount;
+    }
+
+    print(){
+        console.log(this.count);
+    }
+}
+
+const counter = new Counter(0);
+
+counter.add(5)
+counter.add(10)
+
+counter.print();
