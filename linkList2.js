0fefb9336ff45fe16a1e163bcbda9e92e9ab32dc
md-alamin2 <mdalamin22671@gmail.com>
2025-11-10 22:44:33 +0600

    learn about linked list append oppration

---
+++ linkList2.js
@@ -0,0 +1,55 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    // if the linked list is empty
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // if the linked list is not empty
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+  }
+
+  prepend() {}
+
+  insert() {}
+
+  remove() {}
+
+  print() {
+    const arr = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    console.log(arr.join(" -> "), "-> null");
+  }
+}
+
+const linkedList = new LinkList();
+
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+linkedList.print();
